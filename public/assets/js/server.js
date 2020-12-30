66bcb468a0bf0403d751ff446042cfa5ff008445
John Hauth <jkhauth@gmail.com>
2020-12-30 14:32:49 -0500

    specific notes being picked up by server

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,5 +1,6 @@
 //server info
 // ====================================
+const { json } = require("express");
 var express = require("express");
 var fs = require('fs')
 var path = require('path')
@@ -7,7 +8,6 @@ var path = require('path')
 var db = require("../../../db/db.json")
 var app = express();
 var PORT = 3000;
-
 //======================================
 app.use(express.static(path.join(__dirname, "../../../public")));
 
@@ -26,8 +26,28 @@ app.get("/api/notes", function(req, res){
     res.json(db)
 })
 
+// shows specific note
+app.get("/api/notes/:db?", function(req, res){
+    
+    var chosen = req.params.db
+
+    if(chosen){
+    for (var i=0; i<db.length; i++) {
+        if (chosen === db[i].noteID){
+            res.json(db[i]);
+            return;
+        }
+    }
+    res.send("No Note Found");
+
+}else {
+    res.json(db);
+}
+});
+
 // ======================================
 
 app.listen(PORT, function(){
     console.log("app is listening on port " + PORT)
+    console.log(db[1].noteID)
 })
